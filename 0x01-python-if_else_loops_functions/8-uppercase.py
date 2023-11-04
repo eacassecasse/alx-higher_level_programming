797394855fbd033b29321a894a38da6274b6be36
Edmilson de Azevedo Cassecasse <2015edmilsonb1327@gmail.com>
2023-11-03 17:34:56 -0700

    feat: many programs at once

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for o in str:
+        if ord(o) >= 97 and ord(o) <= 122:
+            o = chr(ord(o) - 32)
+       
+        print("{}".format(o), end = "")
+       
+    print("")   
