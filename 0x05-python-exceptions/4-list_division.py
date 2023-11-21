c6d5fb0c8498bda2ac811cc657177cb81cb019e6
Edmilson de Azevedo Cassecasse <2015edmilsonb1327@gmail.com>
2023-11-20 17:46:25 -0800

    feat: working with exceptions - mandatory exercises

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    
+    nlist = []
+    
+    for i in range(0, list_length):
+        try:
+            q = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            q = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            q = 0
+        except IndexError:
+            print("out of range")
+            q = 0
+        finally:
+            nlist.append(q)
+    return (q)
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+This folder holds all the answers for Excepions task
