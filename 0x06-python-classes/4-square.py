cba65914ed2b50745ccfccb1f11e40d2e9a07b20
Edmilson de Azevedo Cassecasse <2015edmilsonb1327@gmail.com>
2023-11-21 13:21:33 -0800

    refactor: coding style

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,34 +1,32 @@
 #!/usr/bin/python3
 
-"""Definition a Square class"""
+"""Define a class Square."""
 
 
 class Square:
-    """Represents a square."""
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """Initializes the Square."""
+        """Initialize a new square.
 
-        self.__size = size
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
 
     @property
     def size(self):
-        """Getter for size property"""
-
-        return self.__size
+        """Get/set the current size of the square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """Setter for size property"""
-
-        if not type(value) is int:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-
     def area(self):
-        """Compute and return area of a square"""
-
-        return self.__size**2
+        """Return the current area of the square."""
+        return (self.__size ** 2)
