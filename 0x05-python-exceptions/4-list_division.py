1c3c6b8edc9e891b9a015ff03339fe0c55c322fb
Edmilson de Azevedo Cassecasse <2015edmilsonb1327@gmail.com>
2023-11-20 18:28:31 -0800

    feat: advanced tasks - print in stderr, python bytecode, cpython

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,9 +2,8 @@
 
 
 def list_division(my_list_1, my_list_2, list_length):
-    
+   ''' Create a list with the results of divisions ''' 
     nlist = []
-    
     for i in range(0, list_length):
         try:
             q = my_list_1[i] / my_list_2[i]
