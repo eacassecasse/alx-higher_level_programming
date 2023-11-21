5c23621f7d8db5fc5471374589486883432c5f32
Edmilson de Azevedo Cassecasse <2015edmilsonb1327@gmail.com>
2023-11-21 12:52:11 -0800

    refactor: optimize readability by ading documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
+"""Definition a Square class"""
+
+
 class Square:
-    """Represents a square and is empty
-    """ 
+    """Represents a square and is empty"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+"""Definition a Square class"""
+
+
 class Square:
     """Represents a square."""
     def __init__(self, size):
