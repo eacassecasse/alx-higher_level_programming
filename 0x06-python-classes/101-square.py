3195fbe5c4ccfd5dfd64a9853465a1cfbf2d049c
Edmilson de Azevedo Cassecasse <2015edmilsonb1327@gmail.com>
2023-11-21 17:20:49 -0800

    refactor: removing whitespaces

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -58,7 +58,7 @@ class Square:
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
-    
+ 
     def __str__(self):
         """Default representation of a Square."""
         if self.__size != 0:
