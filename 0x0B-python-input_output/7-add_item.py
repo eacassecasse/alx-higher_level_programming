96fa817f2924b1049eeb33d846f01939fd55eff4
Edmilson de Azevedo Cassecasse <55765693+eacassecasse@users.noreply.github.com>
2024-01-02 17:07:02 +0200

    fix: Changing ``import sys`` position

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""Append Python arguments into a text file"""
+import sys
 
 if __name__ == "__main__":
-    import sys
 
     load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
