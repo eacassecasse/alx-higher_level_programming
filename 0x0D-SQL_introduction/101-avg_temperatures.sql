d2f9d455543bffb8c5aaf2db37f266fc5b78745c
Edmilson de Azevedo Cassecasse <2015edmilsonb1327@gmail.com>
2024-01-16 13:31:55 -0800

    refactor: Adding a new line at the end of file

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -4,3 +4,4 @@ SELECT city, AVG(value) as avg_temp
 FROM temperatures
 GROUP BY city
 ORDER BY avg_temp DESC;
+
--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -6,3 +6,4 @@ FROM temperatures
 WHERE month = 7 or month = 8
 GROUP BY city
 ORDER BY avg_temp DESC LIMIT 3;
+
