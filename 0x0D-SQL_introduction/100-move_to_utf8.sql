44d0daac5a009888e9f1275d03fc13983d94ca1c
Edmilson de Azevedo Cassecasse <55765693+eacassecasse@users.noreply.github.com>
2024-01-16 23:32:14 +0200

    fix: Changing Database Name to match the requirement

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,7 +2,7 @@
 -- column ``name`` from ``second_table`` to UTF-8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
-USE hbtn_0c_c;
+USE hbtn_0c_0;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE first_table CHANGE name name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
