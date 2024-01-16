60b406bfaf8fc935637c977770c6725bf52045b1
Edmilson de Azevedo Cassecasse <55765693+eacassecasse@users.noreply.github.com>
2024-01-16 23:45:56 +0200

    fix: changing script to convert first_table to UTF-8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,9 +1,4 @@
 -- Converts database ``hbtn_0c_0``, table ``first_table`` and
 -- column ``name`` from ``second_table`` to UTF-8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-
 USE hbtn_0c_0;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
-ALTER TABLE first_table CHANGE name name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
