aa266e11e8b4fb4a28f2f567c097376b6d8ac018
Edmilson Cassecasse <2015edmilsonb1327@gmail.com>
2024-02-14 04:05:05 -0800

    refactor: fix semistandard compliances

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-    return list.map((el, index, array) => {
-        return array[array.length - 1 - index];
-    })
-}
+  return list.map((el, index, array) => {
+    return array[array.length - 1 - index];
+  });
+};
