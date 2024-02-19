f7fc763b1f2673988d8147bb6ed20a81b51d83cc
Edmilson de Azevedo Cassecasse <55765693+eacassecasse@users.noreply.github.com>
2024-02-19 15:05:09 +0200

    fix: Updating model classes

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     for instance in (session.query(State.name, City.id, City.name)
-            .filter(State.id == City.state_id)):
+                            .filter(State.id == City.state_id)):
         print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2])
