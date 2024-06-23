5a7dc5662fbf71e497be0f7f7f2daf3302cbc834
Edmilson de Azevedo Cassecasse <2015edmilsonb1327@gmail.com>
2024-06-23 14:01:04 -0700

    fix: Adding a string in order to split the line 11 to correct PEP8 E128 Indent issue

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -8,8 +8,8 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    estr = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine(estr.format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
