a0f725d4d856a8283c9c273b17d26063dc10f3f0
Edmilson de Azevedo Cassecasse <2015edmilsonb1327@gmail.com>
2024-07-05 03:27:34 -0700

    fix: Correcting the implementation of the method to use requests instear of urllib.request

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,11 +5,12 @@
 - of the X-Request-Id variable found in the header ofthe response.
 """
 import sys
-import urllib.request
+import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
+    email = sys.argv[2]
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    response = requests.post(url, data={'email': email})
+    
+    print(response.text)
